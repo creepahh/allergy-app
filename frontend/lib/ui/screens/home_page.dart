2b4b644a17f3f3908b9584ae8b4a509fbe2bc4a2
Nihal upreti <n.ytuchiha@gmail.com>
2024-01-13 09:42:11 +0545

    home page ui

--- frontend/lib/ui/screens/home_page.dart
+++ frontend/lib/ui/screens/home_page.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_onboarding/constants.dart';
-import 'package:flutter_onboarding/models/plants.dart';
+import 'package:flutter_onboarding/models/dummy.dart';
 import 'package:flutter_onboarding/ui/screens/detail_page.dart';
-import 'package:flutter_onboarding/ui/screens/signin_page.dart';
 import 'package:flutter_onboarding/ui/screens/widgets/plant_widget.dart';
-import 'package:flutter_onboarding/util/ip_address.dart';
 import 'package:page_transition/page_transition.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../../util/ip_address.dart';
 
-var emaila;
-
-class WidgetB {
-  static late Function(String) setEmail = (String email) {};
-
-  // Other WidgetB code...
-
-  // Example function that uses the email received from SignIn
-  static void someFunction(String email) {
-    print("Email received in WidgetB: $email");
-  }
-}
+// Mapping of allergen names to image paths
+Map<String, String> allergenToImagePath = {
+  'milk': 'assets/images/fish.png',
+  'soy': 'assets/images/fish.png',
+  // Add more mappings here
+};
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -32,34 +24,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    int selectedIndex = 0;
-    Size size = MediaQuery.of(context).size;
-
-    // Create an async function to fetch and populate _plantList
-    List<Food> _plantList = [];
-
-    void fetchData() async {
-      String userEmail = 'dummy';
-      _plantList = await Food.fetchFoods(userEmail);
-    }
-
-    // Call the fetchData function
-    fetchData();
-
-    //Plants category
-    List<String> _plantTypes = [
-      'Recommended',
-      'Indoor',
-      'Outdoor',
-      'Garden',
-      'Supplement',
-    ];
-
-    Future<void> fetchUserAllergy(String email) async {
-      String apiUrl = "http://${Globals.ipAddress}:8000/api/user_info/";
-
+  Future<List<String>> fetchAllergens(String email) async {
+    String apiUrl = "http://${Globals.ipAddress}:8000/api/user_info/";
+    try {
       final response = await http.post(
         Uri.parse(apiUrl),
         headers: <String, String>{
@@ -68,127 +35,173 @@ class _HomePageState extends State<HomePage> {
         body: jsonEncode(<String, String>{'email': email}),
       );
 
-      if (response.statusCode == 201) {
-        // Parse the JSON response
-        final Map<String, dynamic> responseData = json.decode(response.body);
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
 
-        print("Response Object: $responseData");
+      if (response.statusCode == 201) {
+        final responseData = json.decode(response.body);
+        if (responseData.containsKey('allergens')) {
+          List<dynamic> allergens = responseData['allergens'];
+          List<String> allergenNames =
+              allergens.map((a) => a['allergens_name'].toString()).toList();
+
+          // Print the allergen names
+          print('Allergen names: $allergenNames');
+
+          return allergenNames;
+        } else {
+          print('Allergens key not found in response');
+          return [];
+        }
       } else {
-        print("Failed to register user. Status code: ${response.statusCode}");
-        // Handle registration failure as needed
+        print('Failed to fetch allergens. Status code: ${response.statusCode}');
+        return [];
       }
+    } catch (e) {
+      print('Error fetching allergens: $e');
+      return [];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    String userEmail = 'niajh.edd@gmail.com'; // Example email
+    List<Plant> _plantList = Plant.plantList;
+
+    bool toggleIsFavorated(bool isFavorited) {
+      return !isFavorited;
     }
 
-    fetchUserAllergy("niajh.edd@gmail.com");
-
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(top: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: SingleChildScrollView(
+        child: FutureBuilder<List<String>>(
+          future: fetchAllergens(userEmail),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('No allergens found'));
+            }
+
+            List<String> allergenNames = snapshot.data!;
+            List<Widget> allergenImages = allergenNames.map((name) {
+              String imagePath =
+                  allergenToImagePath[name] ?? 'assets/images/default.png';
+              return Image.asset(imagePath,
+                  height: 100, width: 100); // Adjust size as needed
+            }).toList();
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                  ),
-                  width: size.width * .9,
+                  padding: const EdgeInsets.only(top: 20),
                   child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(
-                        Icons.search,
-                        color: Colors.black54.withOpacity(.6),
-                      ),
-                      const Expanded(
-                          child: TextField(
-                        showCursor: false,
-                        decoration: InputDecoration(
-                          hintText: 'Search Food',
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0,
                         ),
-                      )),
-                      // Icon(
-                      //   Icons.mic,
-                      //   color: Colors.black54.withOpacity(.6),
-                      // ),
+                        width: size.width * .9,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.search,
+                              color: Colors.black54.withOpacity(.6),
+                            ),
+                            const Expanded(
+                                child: TextField(
+                              showCursor: false,
+                              decoration: InputDecoration(
+                                hintText: 'Search food',
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                              ),
+                            )),
+                            Icon(
+                              Icons.mic,
+                              color: Colors.black54.withOpacity(.6),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          color: Constants.primaryColor.withOpacity(.1),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      )
                     ],
                   ),
-                  decoration: BoxDecoration(
-                    color: Constants.primaryColor.withOpacity(.1),
-                    borderRadius: BorderRadius.circular(20),
+                ),
+
+                SizedBox(
+                  height: size.height * .3,
+                  child: ListView.builder(
+                    itemCount: allergenNames.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      String name = allergenNames[index];
+                      String imagePath = allergenToImagePath[name] ??
+                          'assets/images/default.png';
+                      return Container(
+                        width: 100, // Adjusted size
+                        height: 100, // Adjusted size
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        child: CircleAvatar(
+                          radius: 75, // Adjusted radius
+                          backgroundImage: AssetImage(imagePath),
+                          backgroundColor: Colors
+                              .transparent, // To remove any default background color
+                        ),
+                      );
+                    },
                   ),
-                )
-              ],
-            ),
-          ),
-          //allergens avatar
-          SizedBox(
-            height: size.height * .1,
-            child: ListView.builder(
-              itemCount: _plantList.length, // Change this to your list count
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                // Your data model will be used here instead of _plantList
-                return GestureDetector(
-                  onTap: () {
-                    // Handle the tap event
-                  },
-                  child: Container(
-                    width: 50, // Define the size of the circle
-                    height: 50, // Match the width to create a perfect circle
-                    margin: const EdgeInsets.symmetric(horizontal: 10),
-                    decoration: BoxDecoration(
-                      shape:
-                          BoxShape.circle, // This makes the container circular
-                      image: DecorationImage(
-                        image: AssetImage(_plantList[index]
-                            .imageURL), // Replace with the URL of your image
-                        fit: BoxFit.cover,
-                      ),
+                ),
+
+                Container(
+                  padding: const EdgeInsets.only(left: 16, bottom: 20, top: 20),
+                  child: const Text(
+                    'Scan History',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
                     ),
                   ),
-                );
-              },
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 16, bottom: 20, top: 20),
-            child: const Text(
-              'Scan History',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18.0,
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            height: size.height * .5,
-            child: ListView.builder(
-                itemCount: _plantList.length,
-                scrollDirection: Axis.vertical,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            PageTransition(
-                                child:
-                                    DetailPage(plantId: _plantList[index].id),
-                                type: PageTransitionType.bottomToTop));
-                      },
-                      child: PlantWidget(index: index, plantList: _plantList));
-                }),
-          ),
-        ],
+                ),
+                // Container(
+                //   padding: const EdgeInsets.symmetric(horizontal: 12),
+                //   height: size.height * .5,
+                //   child: ListView.builder(
+                //       itemCount: _plantList.length,
+                //       scrollDirection: Axis.vertical,
+                //       physics: const BouncingScrollPhysics(),
+                //       itemBuilder: (BuildContext context, int index) {
+                //         return GestureDetector(
+                //           onTap: () {
+                //             Navigator.push(
+                //                 context,
+                //                 PageTransition(
+                //                     child: DetailPage(
+                //                         plantId: _plantList[index].plantId),
+                //                     type: PageTransitionType.bottomToTop));
+                //           },
+                //           child:
+                //               PlantWidget(index: index, plantList: _plantList),
+                //         );
+                //       }),
+                // ),
+
+                // ... other widgets ...
+              ],
+            );
+          },
+        ),
       ),
-    ));
+    );
   }
 }
--- frontend/lib/util/ip_address.dart
+++ frontend/lib/util/ip_address.dart
@@ -1,3 +1,4 @@
 class Globals {
-  static String ipAddress = '192.168.1.11';
+  // static String ipAddress = '192.168.1.11';
+  static String ipAddress = '192.168.23.25';
 }
